42c5438226cd21b3402dacf29fe3d3a58b94ee32
rm-reins <rm_reins@icloud.com>
2024-03-18 01:18:11 +0100

    feat: profile page

--- app/(dashboard)/profile/page.jsx
+++ app/(dashboard)/profile/page.jsx
@@ -1,4 +1,18 @@
+"use client";
+import { UserProfile, useAuth } from "@clerk/nextjs";
+
 function ProfilePage() {
-  return <div>ProfilePage</div>;
+  const { signOut } = useAuth();
+  return (
+    <div className="grid gap-9">
+      <button
+        onClick={signOut}
+        className="btn btn-primary sm:hidden justify-self-stretch mx-8 text-lg "
+      >
+        Log out
+      </button>
+      <UserProfile />
+    </div>
+  );
 }
 export default ProfilePage;
