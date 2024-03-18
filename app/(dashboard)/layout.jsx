a55a0b65d20836bd6463c02e06fe4cb23c6e6abf
rm-reins <rm_reins@icloud.com>
2024-03-18 01:17:56 +0100

    style: change positioning

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -5,7 +5,9 @@ export default function layout({ children }) {
   return (
     <div>
       <Navbar />
-      <div className="bg-base-200 px-8 py-12 min-h-screen">{children}</div>
+      <div className="bg-base-200 px-8 py-12 min-h-screen z-0 flex justify-center">
+        {children}
+      </div>
       <BottomNav />
     </div>
   );
