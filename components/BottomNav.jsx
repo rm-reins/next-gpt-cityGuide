993573733af1deaa23a1af07cf47a03afe5c1ee7
rm-reins <rm_reins@icloud.com>
2024-03-18 01:18:50 +0100

    feat: bottom navbar functionality

--- components/BottomNav.jsx
+++ components/BottomNav.jsx
@@ -1,3 +1,6 @@
+"use client";
+import Link from "next/link.js";
+import { useState } from "react";
 import {
   MdOutlineChat,
   MdOutlineLocationOn,
@@ -5,25 +8,59 @@ import {
   MdOutlineAccountCircle,
 } from "react-icons/md";
 
+const links = [
+  { href: "/chat", label: "Chat", icon: <MdOutlineChat className="w-6 h-6" /> },
+  { href: "/tours", label: "Tours", icon: <MdOutlineLocationOn className="w-6 h-6" /> },
+  {
+    href: "/tours/new-tour",
+    label: "New Tour",
+    icon: <MdOutlineAddLocationAlt className="w-6 h-6" />,
+  },
+  {
+    href: "/profile",
+    label: "Profile",
+    icon: <MdOutlineAccountCircle className="w-6 h-6" />,
+  },
+];
+
 function BottomNav() {
+  const [isActive, setIsActive] = useState(null);
+
+  function handleClick(index) {
+    setIsActive((isActive) => (isActive === index ? null : index));
+  }
+
   return (
     <div className="btm-nav btm-nav-md sm:hidden">
-      <button>
+      {links.map((link, index) => {
+        return (
+          <Link
+            href={link.href}
+            className={isActive === index ? "active" : null}
+            key={index}
+            onClick={() => handleClick(index)}
+          >
+            {link.icon}
+            {link.label}
+          </Link>
+        );
+      })}
+      {/* <Link href="/chat">
         <MdOutlineChat className="w-6 h-6" />
         Chat
-      </button>
-      <button className="active">
+      </Link>
+      <Link href="/tours" className="active">
         <MdOutlineLocationOn className="w-6 h-6" />
         Tours
-      </button>
-      <button>
+      </Link>
+      <Link href="/tours/new-tour">
         <MdOutlineAddLocationAlt className="w-6 h-6" />
         New Tour
-      </button>
-      <button>
+      </Link>
+      <Link href="profile">
         <MdOutlineAccountCircle className="w-6 h-6" />
-        Account
-      </button>
+        Profile
+      </Link> */}
     </div>
   );
 }
