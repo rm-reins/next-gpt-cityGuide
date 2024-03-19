9ac491f4ad74b03340ce9bfb188de4a699c5ad8b
rm-reins <rm_reins@icloud.com>
2024-03-20 00:05:01 +0100

    feat: chat finished

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -3,26 +3,82 @@
 import chatResponse from "@/utils/actions.js";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import toast from "react-hot-toast";
+import { useUser } from "@clerk/nextjs";
 
 function Chat() {
   const [inputText, setInputText] = useState("");
-  const [message, setMessage] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const { user } = useUser();
+  const date = new Date();
+  const local = date.toLocaleTimeString("en-DE", {
+    timeStyle: "short",
+    hourCycle: "h24",
+  });
 
-  const { mutate } = useMutation({
-    mutationFn: (message) => chatResponse(message),
+  const { mutate, isPending } = useMutation({
+    mutationFn: (query) => chatResponse([...messages, query]),
+    onSuccess: (data) => {
+      if (!data) {
+        toast.error("Something went wrong...");
+        return;
+      }
+      setMessages((prev) => [...prev, data]);
+    },
   });
 
   function handleSubmit(e) {
     e.preventDefault();
-    mutate(inputText);
+    const query = { role: "user", content: inputText };
+    mutate(query);
+    setMessages((prev) => [...prev, query]);
+    setInputText("");
   }
 
   return (
-    <div className="grid grid-rows-[1fr, auto]">
-      <div>
-        <h2 className="text-5xl">messages</h2>
+    <div className="flex flex-col justify-between max-w-2xl w-full h-full">
+      <div className="min-h-80 overflow-auto">
+        {messages.map(({ role, content }, index) => {
+          return (
+            <div key={index}>
+              {role === "user" ? (
+                <div className="chat chat-end">
+                  <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                      <img alt="user_photo" src={user.imageUrl} />
+                    </div>
+                  </div>
+                  <div className="chat-header">
+                    {user.firstName}
+                    <time className="text-xs opacity-50">{` ${local}`}</time>
+                  </div>
+
+                  <div className="chat-bubble">{content}</div>
+                </div>
+              ) : (
+                <div className="chat chat-start">
+                  <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                      <img
+                        alt="AI_photo"
+                        src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                      />
+                    </div>
+                  </div>
+                  <div className="chat-header">
+                    UrbanIQ AI
+                    <time className="text-xs opacity-50">{` ${local}`}</time>
+                  </div>
+                  <div className="chat-bubble">{content}</div>
+                </div>
+              )}
+            </div>
+          );
+        })}
+        {isPending ? <span className="loading"></span> : null}
       </div>
-      <form onSubmit={handleSubmit} className="max-w-4xl pt-12">
+
+      <form onSubmit={handleSubmit} className="min-w-96 max-w-4xl pt-12">
         <div className="join w-full">
           <input
             type="text"
@@ -32,8 +88,13 @@ function Chat() {
             required
             onChange={(e) => setInputText(e.target.value)}
           />
-          <button className="btn btn-primary join-item" type="submit">
-            Ask question
+
+          <button
+            className="btn btn-primary join-item"
+            type="submit"
+            disabled={isPending}
+          >
+            {isPending ? "Processing..." : "Ask question"}
           </button>
         </div>
       </form>
--- utils/actions.js
+++ utils/actions.js
@@ -1,3 +1,27 @@
 "use server";
+import OpenAI from "openai";
 
-export default async function chatResponse(chatMessage) {}
+const openAI = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export default async function chatResponse(messages) {
+  try {
+    const resp = await openAI.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a city guide helper. Your purpose is to assist users in discovering attractive places to visit in their specified city.",
+        },
+        ...messages,
+      ],
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+    });
+    return resp.choices[0].message;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
