7a1d57ec3e695a4056689bfd0c140e1aaeb05e91
rm-reins <rm_reins@icloud.com>
2024-03-20 23:56:31 +0100

    misc: timeline update

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -2,7 +2,7 @@ function Timeline() {
   return (
     <ul className="timeline timeline-vertical">
       <li>
-        <div className="timeline-start timeline-box">Landing</div>
+        <div className="timeline-start timeline-box">Chat</div>
         <div className="timeline-middle">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -17,30 +17,30 @@ function Timeline() {
             />
           </svg>
         </div>
-        <hr className="bg-primary" />
-      </li>
-      <li>
-        <hr className="bg-primary" />
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-5 h-5 text-primary"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        <div className="timeline-end timeline-box">Navbar</div>
-        <hr className="bg-primary" />
+        <hr className="bg-current" />
       </li>
       <li>
         <hr className="" />
-        <div className="timeline-start timeline-box">Sidebar</div>
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5 text-primary"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-end timeline-box">New Tour</div>
+        <hr className="" />
+      </li>
+      <li>
+        <hr className="" />
+        <div className="timeline-start timeline-box">Tours</div>
         <div className="timeline-middle">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -73,12 +73,12 @@ function Timeline() {
             />
           </svg>
         </div>
-        <div className="timeline-end timeline-box">Member Profile</div>
+        <div className="timeline-end timeline-box">Tours Search</div>
         <hr />
       </li>
       <li>
         <hr />
-        <div className="timeline-start timeline-box">Chat Page</div>
+        <div className="timeline-start timeline-box">...</div>
         <div className="timeline-middle">
           <svg
             xmlns="http://www.w3.org/2000/svg"
