4e5429ce0a6a53a2cf46d49001ce2708605b05bf
rm-reins <rm_reins@icloud.com>
2024-03-22 00:08:51 +0100

    feat: new tour page setup

--- components/TourInfo.jsx
+++ components/TourInfo.jsx
@@ -1,4 +1,20 @@
-function TourInfo() {
-  return <div>TourInfo</div>;
+function TourInfo({ tour }) {
+  const { title, description, places } = tour;
+  return (
+    <div className="max-w-2xl">
+      <h1 className="text-4xl font-semibold mb-4">{title}</h1>
+      <p className="leading-lose mb-6">{description}</p>
+
+      <ul>
+        {places.map((place) => {
+          return (
+            <li key={place} className="mb-4 bg-base-100 p-4 rounded-xl">
+              {place}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 export default TourInfo;
