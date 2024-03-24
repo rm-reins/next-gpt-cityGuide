4800273bce02b8cfc6c072471002f821e4f6ce28
rm-reins <rm_reins@icloud.com>
2024-03-24 17:44:28 +0100

    feat: get image from unsplash

--- app/(dashboard)/tours/[id]/page.jsx
+++ app/(dashboard)/tours/[id]/page.jsx
@@ -3,6 +3,10 @@ import { getTourById } from "@/utils/actions.js";
 import Link from "next/link.js";
 import { redirect } from "next/navigation.js";
 import { MdArrowBackIos } from "react-icons/md";
+import axios from "axios";
+import Image from "next/image.js";
+
+const url = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_KEY}&query=`;
 
 async function SingleTourPage({ params }) {
   const tour = await getTourById(params.id);
@@ -11,12 +15,27 @@ async function SingleTourPage({ params }) {
     redirect("/torus");
   }
 
+  const { data } = await axios.get(`${url}${tour.city}`);
+  const tourImage = data?.results[0]?.urls?.raw;
+
   return (
     <div className="flex flex-col">
       <Link href="/tours" className="btn btn-primary mb-12 text-bold sm:max-w-40 ">
         <MdArrowBackIos />
         Back to Tours
       </Link>
+      {tourImage ? (
+        <div>
+          <Image
+            src={tourImage}
+            width={300}
+            height={300}
+            className="rounded-xl shadow-xl mb-16 w-96 object-cover"
+            alt={tour.title}
+            priority
+          />
+        </div>
+      ) : null}
       <TourInfo tour={tour} />
     </div>
   );
